5b8463012e597be5484112215320f8666ffc71bc
Sophie Cheng <sophiecheng181@gmail.com>
2023-07-31 15:16:59 -0400

    Initial Commit

---
+++ Beeware/BeewareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeewareApp.swift
+//  Beeware
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BeewareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
